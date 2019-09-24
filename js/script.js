6fde864873f96f9c1502dcfa04004d2606f0cb40
Debasri Dasgupta <debasri_dasgupta@rediffmail.com>
2019-09-24 02:21:25 +0200

    Added animations

--- js/script.js
+++ js/script.js
@@ -6,40 +6,49 @@ jQuery(document).ready(function($) {
 
     $("#avatar-picker-box").find('a').hover( function() {
       $this = $(this);
-      $imgelement = $(this).find('div[id="div-content"]');
-        $imgelement.animate({
-          top: "+=50",
-          opacity: 0
-        }, 0, function() {
-            $imgelement.removeClass("icon-bg").addClass("loader").css({top: "-=100", opacity: 1}).animate({
-              top: "+=50"
-            });
-        });
-        
+      $imgelement = $(this).find('div.wrapper');
+      $imgelement.removeClass("wrapper").addClass("wrapper-icon-box-selected");      
     },
     function() {
       $this = $(this);
-      $imgelement = $(this).find('div[id="div-content"]');
-      $imgelement.removeClass("loader").addClass("icon-bg");
+      $imgelement = $(this).find('div.wrapper-icon-box-selected');
+      $imgelement.removeClass("wrapper-icon-box-selected").addClass("wrapper");
      
     });
 
     $("#avatar-picker-box").find('a').on('click', function() {
       $this = $(this);
       $button = $('div[data-toolbar="avatar-picker-box"]').find('div[id="div-wrapper-selected"]');
-      $newImg = $(this).find('div[id="div-wrapper"]').attr("class");
+      $newImg = $(this).find('div.wrapper-icon-box-selected').attr("class");
       $oldImg = $button.attr("class");
+      $imgelement = $(this).find('div[id="div-content"]');
       if($newImg != $oldImg) {
-        $button.animate({
+        $imgelement.animate({
           top: "+=50",
           opacity: 0
-        }, 200, function() {
-            $button.removeClass($oldImg).addClass($newImg).css({top: "-=100", opacity: 1}).animate({
-            top: "+=50"
-          });
-        });
-      }
-
+        }, 0, function() {
+            $imgelement.removeClass("icon-bg").addClass("loader").css({top: "-=102", left: "-=3", opacity: 1}).animate({
+              top: "+=50"
+            },0, setTimeout( function(){
+                    $button.animate({
+                    top: "+=50",
+                    opacity: 0
+                    }, 0, function() {
+                        $button.removeClass($oldImg).addClass($newImg).css({top: "-=100", opacity: 1}).animate({
+                        top: "+=50"
+                    },0, function() {
+                        $imgelement.removeClass("loader").addClass("icon-bg").css({})
+                    }
+                    );
+                    }
+                    );
+            } ,1000)
+            );
+        }
+        
+        )} else {
+            $imgelement.removeClass("loader").addClass("icon-bg").css({});
+        }
     });
 
     $('div[data-toolbar="avatar-picker-box"]').toolbar({
